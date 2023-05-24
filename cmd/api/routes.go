ac49858a205f417fccba1735ee80f1206a6a069b
Brett <brett.james@portmandental.co.uk>
2023-05-24 16:40:10 +0100

    implementing handlers, routes + creating Dockerfile

--- cmd/api/routes.go
+++ cmd/api/routes.go
@@ -9,17 +9,19 @@ import (
 )
 
 func (app *Config) routes() http.Handler {
-	mux := chi.NewRouter
-	mux.Use(cors.handler(cors.Options{
-		AllowedOrigins: []string{"https://*", "http://*"},
-		AllowedMethods: []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
-		AllowedHeaders: []string{"Accept", "Authorization", "Content-Type", "X-CSRF-Token"},
-		ExposedHeaders: []string{"Link"},
-		AllowedCredentials: true,
-		MaxAge: 300,
+	mux := chi.NewRouter()
+	mux.Use(cors.Handler(cors.Options{
+		AllowedOrigins:   []string{"https://*", "http://*"},
+		AllowedMethods:   []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
+		AllowedHeaders:   []string{"Accept", "Authorization", "Content-Type", "X-CSRF-Token"},
+		ExposedHeaders:   []string{"Link"},
+		AllowCredentials: true,
+		MaxAge:           300,
 	}))
 
 	mux.Use(middleware.Heartbeat("/ping"))
+
+	mux.Post("/", app.Broker)
 
 	return mux
 }
--- go.mod
+++ go.mod
@@ -1,6 +1,6 @@
-module broker
+module github.com/brettjames250/broker-service
 
-go 1.20
+go 1.18
 
 require (
 	github.com/go-chi/chi v1.5.4
